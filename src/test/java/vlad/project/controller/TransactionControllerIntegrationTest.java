c7ea30f528a79f9f00b7ef7b2963b7fa279f313e
Vlad <vladislavcherkashin98@gmail.com>
2020-11-15 21:35:54 +0200

    creating connection between branches

--- src/test/java/vlad/project/controller/TransactionControllerIntegrationTest.java
+++ src/test/java/vlad/project/controller/TransactionControllerIntegrationTest.java
@@ -1,5 +1,28 @@
 package vlad.project.controller;
 
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.web.servlet.MockMvc;
+import vlad.project.service.TransactionService;
+
+
+
+@WebMvcTest
 public class TransactionControllerIntegrationTest {
+	
+	@Autowired
+	private MockMvc mockMvc;
+	
+	@MockBean
+	private TransactionService transactionService;
+	
+	@Test
+	public void transactionsEndpointShouldReturnTransactionsViaService() {
+		
+		
+		
+	}
 
 }
